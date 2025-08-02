b31297c3fb2ce44ae5ffb2b1472fee084771f5de
muahe2004 <lyvanminh280504@gmail.com>
2025-08-03 03:04:59 +0700

    update: api end points

--- backend/app/main.py
+++ backend/app/main.py
@@ -5,13 +5,5 @@ from app.api.main import api_router
 
 app = FastAPI()
 
-@app.on_event("startup")
-def on_startup():
-    with Session(engine) as session:
-        init_db(session)
+app.include_router(api_router, prefix="/ums/api")
 
-@app.get("/")
-def read_root():
-    return {"message": "Zalo: 033426636"}
-
-app.include_router(api_router)
