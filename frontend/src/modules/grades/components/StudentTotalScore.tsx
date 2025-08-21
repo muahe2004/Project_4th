11a8c99e6ac4593bf8fb460d30aa8ef799e223cc
muahe2004 <lyvanminh280504@gmail.com>
2025-08-21 23:26:04 +0700

    feat: UI grades page

---
+++ frontend/src/modules/grades/components/StudentTotalScore.tsx
@@ -0,0 +1,72 @@
+import { Box, Table, TableBody, TableCell, TableContainer, TableHead, TableRow } from "@mui/material";
+import Paper from "@mui/material/Paper";
+import "./styles/studentTableScore.css";
+
+function createData(
+  student_code: string,
+  name: string,
+  grade4: string,
+  grade10: string,
+  gpa4: string,
+  accumulated_gpa4: string,
+  accumulated_gpa10: string,
+  accumulated_credits: string,
+  studied_credits: string
+) {
+  return { student_code, name, grade4, grade10, gpa4, accumulated_gpa4, accumulated_gpa10, accumulated_credits, studied_credits };
+}
+
+const rows = [
+  createData(
+    "10122256",
+    "Lý Văn Minh",
+    "Giỏi",
+    "Giỏi",
+    "3.38",
+    "3.38",
+    "8.28",
+    "112 / 112",
+    "112"
+  ),
+];
+
+export function StudentTotalScore() {
+  return (
+    <Box className="student-tableScore">
+      <TableContainer className="primary-table-container" component={Paper}>
+        <Table className="primary-table" aria-label="student total score table">
+          <TableHead className="primary-thead">
+            <TableRow className="primary-trow">
+              <TableCell className="primary-thead__cell" align="left">MSV</TableCell>
+              <TableCell className="primary-thead__cell" align="left">Họ và tên</TableCell>
+              <TableCell className="primary-thead__cell" align="center">Xếp loại H4</TableCell>
+              <TableCell className="primary-thead__cell" align="center">Xếp loại H10</TableCell>
+              <TableCell className="primary-thead__cell" align="center">TBC H4</TableCell>
+              <TableCell className="primary-thead__cell" align="center">TBC H4</TableCell>
+              <TableCell className="primary-thead__cell" align="center">TBC H10</TableCell>
+              <TableCell className="primary-thead__cell" align="center">Số tín chỉ tích lũy</TableCell>
+              <TableCell className="primary-thead__cell" align="center">Số tín chỉ học tập</TableCell>
+            </TableRow>
+          </TableHead>
+          <TableBody className="primary-tbody">
+            {rows.map((row) => (
+              <TableRow key={row.student_code} className="primary-trow">
+                <TableCell className="primary-tcell" align="left">{row.student_code}</TableCell>
+                <TableCell className="primary-tcell" align="left">{row.name}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.grade4}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.grade10}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.gpa4}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.accumulated_gpa4}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.accumulated_gpa10}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.accumulated_credits}</TableCell>
+                <TableCell className="primary-tcell" align="center">{row.studied_credits}</TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </Box>
+  );
+}
+
+export default StudentTotalScore;
--- frontend/src/modules/grades/views/Grades.tsx
+++ frontend/src/modules/grades/views/Grades.tsx
@@ -1,21 +1,104 @@
-import { Box, Toolbar, Typography } from "@mui/material";
+import { Box, FormControl, FormHelperText, InputLabel, MenuItem, Select, Typography } from "@mui/material";
 import { useTranslation } from 'react-i18next';
 
-import { StudentInformation } from "../components/StudentInformation"
+import StudentTotalScore from "../components/StudentTotalScore"
+import ScoresTable from "../components/ScoresTable";
 
 import "./styles/Grades.css";
-import ScoresTable from "../components/ScoresTable";
 
 export function GradesPage() {
     const { t } = useTranslation();
   
     return (
-        <main className="academic-results">
+        <main className="grades">
             <Typography className="primary-title">
                 KẾT QUẢ HỌC TẬP
             </Typography>
 
-            <StudentInformation></StudentInformation>
+            <StudentTotalScore></StudentTotalScore>
+
+            <Box className="grades-flex">
+                <FormControl className="grades-controller">
+                    <InputLabel className="select-primary__label" id="demo-simple-select-helper-label">Học kỳ</InputLabel>
+                    <Select
+                        className="select-primary"
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="Học kỳ"
+                        MenuProps={{
+                            disableScrollLock: true,   
+                        }}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className="grades-controller">
+                    <InputLabel className="select-primary__label" id="demo-simple-select-helper-label">Năm học</InputLabel>
+                    <Select
+                        className="select-primary"
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="Năm học"
+                        MenuProps={{
+                            disableScrollLock: true,   
+                        }}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className="grades-controller">
+                    <InputLabel className="select-primary__label" id="demo-simple-select-helper-label">Ngành học</InputLabel>
+                    <Select
+                        className="select-primary"
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="Ngành học"
+                        MenuProps={{
+                            disableScrollLock: true,   
+                        }}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+
+                <FormControl className="grades-controller">
+                    <InputLabel className="select-primary__label" id="demo-simple-select-helper-label">Học phần</InputLabel>
+                    <Select
+                        className="select-primary"
+                        labelId="demo-simple-select-helper-label"
+                        id="demo-simple-select-helper"
+                        label="Học phần"
+                        MenuProps={{
+                            disableScrollLock: true,   
+                        }}
+                    >
+                        <MenuItem value="">
+                            <em>None</em>
+                        </MenuItem>
+                        <MenuItem value={10}>Ten</MenuItem>
+                        <MenuItem value={20}>Twenty</MenuItem>
+                        <MenuItem value={30}>Thirty</MenuItem>
+                    </Select>
+                </FormControl>
+            </Box>
+
             <ScoresTable></ScoresTable>
         </main>
     );
